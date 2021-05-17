b022f7f96bb86c4872047e4f58e672a6a7f837bf
johnpro21 <gjohnktym@gmail.com>
2021-05-17 06:50:53 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:trackit/SignIn.dart';
+import 'package:trackit/SignUp.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'HomePage.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
+  runApp(MyApp());
+}
+
+// First put json file in android and json
+// use gradle in android plugin
+// yaml for firebase
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Firebase Loogin',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        backgroundColor: Colors.grey[50],
+        appBarTheme: AppBarTheme(color: Colors.white),
+
+        /* light theme settings */
+      ),
+      darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          backgroundColor: Colors.black,
+          appBarTheme: AppBarTheme(color: Colors.black)
+          /* dark theme settings */
+          ),
+      themeMode: ThemeMode.system,
+      home: HomePage(),
+      routes: {
+        '/home': (BuildContext context) => HomePage(),
+        '/signin': (BuildContext context) => SignIn(),
+        '/signup': (BuildContext context) => SignUp(),
+      },
+    );
+  }
+}
